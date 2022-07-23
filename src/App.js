dca582d4d4f2254cac2e2a0fcd8aeec5b66bc067
barrionuevoje <barrionuevoje@gmail.com>
2022-07-23 13:59:35 -0300

    Desafio menu e-commerce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Cabecera from "./componets/Cabecera";
+import Busqueda from "./componets/Busqueda";
+import "bootstrap/dist/css/bootstrap.css";
+import{Link, BrowserRouter, Route, Routes} from "react-router-dom"; 
+import Biblioteca from "./componets/Biblioteca";
+import Listas from "./componets/Listas";
+import TopRanked from "./componets/TopRanked";
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      
+        
+        <Cabecera titulo="Om Accesorios"/>
+          <nav>
+            <ul className="nav bg-dark text-center">
+              <li className='nav-item'><Link className="nav-link" to="/biblioteca">Biblioteca</Link></li>
+              <li className='nav-item'><Link className="nav-link" to="/listas" >Listas</Link></li>
+              <li className='nav-item'><Link className="nav-link" to="/topranked">Top Ranked</Link></li>
+            </ul>
+          </nav>
+        <Busqueda />
+      <main className="container">
+        <Routes>
+          <Route path="/biblioteca" element={<Biblioteca />}/>
+          <Route path="/listas" element={<Listas />}/>
+          <Route path="/topranked" element={<TopRanked />} />
+        </Routes>  
+      </main> 
+      
+    </BrowserRouter>
   );
 }
 
